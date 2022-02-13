d32c0aca8ebf5206a0c0f9372c1ad83ce81234ad
Felipe Borges <bumbleblo2013@gmail.com>
2022-02-13 02:25:23 -0300

    Adding help text and some comments. Renaming main.hs to Main.hs to attend a language pattern
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,17 +1,30 @@
 module Cli where
 
-create :: [String] -> IO()
-create args = 
-    putStrLn "dummy function"
-
-
+-- Register a command to a string
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [
     ("create", create)
     ]
 
+-- Command calls implementation
+create :: [String] -> IO()
+create args = 
+    putStrLn "dummy function"
 
+
+-- Documentation text
+helpText = 
+    "create          Create a note\n"
+
+
+-- Get all arguments from cli and parse then
+-- If there is a empty list of arguments this 
+-- function just shows the helpText in screen
+--
+-- If the command sended by the user don't have
+-- a action match, a error message will be showed
 parseCommands :: [String] -> IO ()
+parseCommands [] = putStrLn helpText
 parseCommands allArgs = do 
 
     let (command:args) = allArgs

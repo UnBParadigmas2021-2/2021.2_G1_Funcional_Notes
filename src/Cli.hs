50af941dd2b9dffd34f6c76539dc7544a69a03f2
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-02-13 19:00:58 -0300

    Integrate the Operations module
    The Operations module has been integrated with the CLI one

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,31 +1,32 @@
-module Cli where
+module Cli (parseCommands) where
+
+import Operations as Ops
 
 -- Register a command to a string
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [
-    ("create", create)
+    ("create", createNote)
     ]
 
 -- Command calls implementation
-create :: [String] -> IO()
-create args = 
-    putStrLn "dummy function"
-
+createNote :: [String] -> IO()
+createNote args =
+    Ops.create
 
 -- Documentation text
-helpText = 
+helpText =
     "create          Create a note\n"
 
 
 -- Get all arguments from cli and parse then
--- If there is a empty list of arguments this 
+-- If there is a empty list of arguments this
 -- function just shows the helpText in screen
 --
 -- If the command sended by the user don't have
 -- a action match, a error message will be showed
 parseCommands :: [String] -> IO ()
 parseCommands [] = putStrLn helpText
-parseCommands allArgs = do 
+parseCommands allArgs = do
 
     let (command:args) = allArgs
     let action = lookup command dispatch

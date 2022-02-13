cf6d9bf40edc4ccc6757162be3e77ff13f3fcdd4
marcelog5 <marcelovictorg2@gmail.com>
2022-02-13 12:55:38 -0300

    Added function to read notes

---
+++ src/Readtxt.hs
@@ -0,0 +1,21 @@
+module Readtxt (readTxt) where
+
+import System.IO
+import System.Directory (doesFileExist)
+
+readTxt :: FilePath -> IO String  
+readTxt path = do
+  content <- readFileIfExist path
+  return content
+
+readFileIfExist :: FilePath -> IO String
+readFileIfExist path = do
+  exist <- doesFileExist path 
+  if exist
+    then readFileThatExist path
+    else return ""
+  
+readFileThatExist :: FilePath -> IO String
+readFileThatExist path = do
+  contents <- readFile path
+  return contents
--- src/main.hs
+++ src/main.hs
@@ -4,10 +4,12 @@ import System.IO
 import Notes
 import Operations
 import Write (write)
+import Readtxt (readTxt)
 
 main :: IO()
 main = do
     -- CLI arguments
     args <- getArgs
     write "Notes/teste.txt" "something\nTeste1\nTeste2"
+    content <- readTxt "Notes/teste.txt"
     create

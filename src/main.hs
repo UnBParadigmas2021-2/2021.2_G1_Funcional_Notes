02fe2b3bc9a47fa911fe1bc015246dc4d625103d
marcelog5 <marcelovictorg2@gmail.com>
2022-02-13 12:54:57 -0300

    Added function to write a note

--- src/main.hs
+++ src/main.hs
@@ -3,10 +3,11 @@ import System.IO
 
 import Notes
 import Operations
+import Write (write)
 
 main :: IO()
 main = do
     -- CLI arguments
     args <- getArgs
-
+    write "Notes/teste.txt" "something\nTeste1\nTeste2"
     create

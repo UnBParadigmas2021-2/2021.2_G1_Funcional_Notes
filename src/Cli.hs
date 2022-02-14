bd8585211dffdcdf9c34abc010ebc95a6f2ea33f
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-02-14 20:06:39 -0300

    Show note and its contents
    Add function to detail a specific note and its contents.

--- src/Cli.hs
+++ src/Cli.hs
@@ -6,11 +6,13 @@ import CreateNote
 import ListNotes
 import ListNotesByTag
 import Deletetxt
+import Show
 
 -- Register a command to a string
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [
     ("create", create),
+    ("show", showOne),
     ("show-all", showAll),
     ("show-tagged", showTagged),
     ("delete", delete)
@@ -20,6 +22,13 @@ dispatch = [
 create :: [String] -> IO()
 create args =
     createNote
+
+showOne :: [String] -> IO()
+showOne args = do
+    let id = readMaybe (head args) :: Maybe Int
+    case id of
+        Just id -> showNote id
+        Nothing -> putStrLn "Invalid note id"
 
 showAll :: [String] -> IO()
 showAll args =
@@ -36,15 +45,15 @@ delete args = do
     case id of
         Just id -> deleteById id
         Nothing -> putStrLn "Invalid note id"
-    --deleteById id
 
 -- Documentation text
 helpText =
     "Usage:\n" ++
     "create\tCreate a note\n" ++
+    "show note-id\tDetail a note with id 'note-id' and its contents\n" ++
     "show-all\tShow all notes\n" ++
     "show-tagged tag\t Show all notes with the tag 'tag'\n" ++
-    "delete note-id\t Delete note by its id\n"
+    "delete note-id\t Delete note with id 'note-id'\n"
 
 -- Get all arguments from cli and parse then
 -- If there is a empty list of arguments this

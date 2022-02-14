fc69699f82c14f82d91c380c1ac5c6a31dfd789c
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-02-14 16:14:16 -0300

    Add options for showing all or tagged notes
    - update the help message with usage text
    - add show-all and show-tagged commands
    - integrate the previously developed functions with imports

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,11 +1,15 @@
 module Cli (parseCommands) where
 
 import CreateNote
+import ListNotes
+import ListNotesByTag
 
 -- Register a command to a string
 dispatch :: [(String, [String] -> IO ())]
 dispatch = [
-    ("create", create)
+    ("create", create),
+    ("show-all", showAll),
+    ("show-tagged", showTagged)
     ]
 
 -- Command calls implementation
@@ -13,10 +17,21 @@ create :: [String] -> IO()
 create args =
     createNote
 
+showAll :: [String] -> IO()
+showAll args =
+    listNotes
+
+showTagged :: [String] -> IO()
+showTagged args = do
+     let tag = head args
+     listNotesByTag tag
+
 -- Documentation text
 helpText =
-    "create          Create a note\n"
-
+    "Usage:\n" ++
+    "create\tCreate a note\n" ++
+    "show-all\tShow all notes\n" ++
+    "show-tagged tag\t Show all notes with the tag 'tag'\n"
 
 -- Get all arguments from cli and parse then
 -- If there is a empty list of arguments this
@@ -32,5 +47,7 @@ parseCommands allArgs = do
     let action = lookup command dispatch
 
     case action of
-        Nothing -> putStrLn "We don't have this command"
+        Nothing -> do
+            putStrLn "We don't have this command\n"
+            putStrLn helpText
         Just action -> action args
